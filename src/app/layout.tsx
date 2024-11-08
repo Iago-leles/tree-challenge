8624e1205f1b9872f20a8650ace6a4361a382b63
iago.leles <iago.leles@gmail.com>
2024-11-08 01:51:13 -0300

    add react suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google"
 
 import "./globals.css";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased flex flex-col h-screen`}>
-        {children}
+        <React.Suspense fallback={<div>Carregando...</div>}>
+          {children}
+        </React.Suspense>
       </body>
     </html>
   );
